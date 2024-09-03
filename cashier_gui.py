45948b2e67622d18738297d9244ea8a017371f5d
unknown <bastigonzales@protonmail.com>
2024-09-03 02:35:18 -0700

    added more commands/buttons
    Afternoon, Evening, How May I help, Do you want anything, Thank you

--- cashier_gui.py
+++ cashier_gui.py
@@ -9,12 +9,53 @@ def speak_text():
     engine = pyttsx3.init()
     engine.say("Hello!")
     engine.runAndWait()
+    engine.stop()
 
-    #Speak Good Morning
+
+#Speak Good Morning
 def speak_morning():
     engine = pyttsx3.init()
     engine.say("Good Morning!")
     engine.runAndWait()
+    engine.stop()
+
+
+#Speak Good Afternoon
+def speak_afternoon():
+    engine = pyttsx3.init()
+    engine.say("Good Afternoon!")
+    engine.runAndWait()
+    engine.stop()
+
+
+#Speak Good Evening
+def speak_evening():
+    engine = pyttsx3.init()
+    engine.say("Good Evening!")
+    engine.runAndWait()
+    engine.stop()
+
+#Speak How May I help
+def speak_help():
+    engine = pyttsx3.init()
+    engine.say("How May I help you?")
+    engine.runAndWait()
+    engine.stop()
+
+
+#Speak Do you want to add anything
+def speak_add():
+    engine = pyttsx3.init()
+    engine.say("Do you want too add anything?")
+    engine.runAndWait()
+    engine.stop()
+
+#Speak Do you want to add anything
+def speak_thanks():
+    engine = pyttsx3.init()
+    engine.say("Thank you!!")
+    engine.runAndWait()
+    engine.stop()
 
 #Window Title
 root = tk.Tk()
@@ -37,14 +78,48 @@ Logo=PhotoImage(file="./images/tonyo_Icon.png")
 Label(Top_frame,image=Logo,bg="#ffffff").place(x=10,y=5)
 
  
+# Create a label and entry field for the text
+text_label = tk.Label(root, text="Preset Conversations:", bg="green", font=("bold", 10))
+text_label.pack()
+text_label.place(x=550,y=80)
+
 #HELLO BUTTON
 speak_button = tk.Button(root, text="Hello", command=speak_text)
 speak_button.pack()
 speak_button.place(x=550,y=110)
 
+#GOOD MORNING BUTTON
 speak_button = tk.Button(root, text="Good Morning", command=speak_morning)
 speak_button.pack()
 speak_button.place(x=550,y=150)
+
+
+#GOOD AFTERNOON BUTTON
+speak_button = tk.Button(root, text="Good Afternoon", command=speak_afternoon)
+speak_button.pack()
+speak_button.place(x=550,y=190)
+
+
+#GOOD EVENING BUTTON
+speak_button = tk.Button(root, text="Good Evening", command=speak_evening)
+speak_button.pack()
+speak_button.place(x=550,y=230)
+
+#HOW MAY I HELP YOU BUTTON
+speak_button = tk.Button(root, text="How May I help you?", command=speak_help)
+speak_button.pack()
+speak_button.place(x=550,y=270)
+
+#DO YOU WANT ANYTHING BUTTON
+speak_button = tk.Button(root, text="Do you want to add anything?", command=speak_add)
+speak_button.pack()
+speak_button.place(x=550,y=310)
+
+
+#THANK YOU BUTTON
+speak_button = tk.Button(root, text="Thank you!!", command=speak_thanks)
+speak_button.pack()
+speak_button.place(x=550,y=350)
 
 
 #Start the main loop
