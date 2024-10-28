213d18e7e5de67dddff501fa5c9d1d4a8c7f14d7
Sebastian Gonzales <bastigonzales@protonmail.com>
2024-10-28 19:53:00 +0800

    added buttons for text-to-speech

--- customtkinter-cashier.py
+++ customtkinter-cashier.py
@@ -1,7 +1,62 @@
 import customtkinter as ctk
+import pyttsx3
 from RealtimeSTT import AudioToTextRecorder
  
+# Initialize TTS engine
+tts_engine = pyttsx3.init()
 
+#Speak Hello
+def speak_hello():
+    engine = pyttsx3.init()
+    engine.say("Hello!")
+    engine.runAndWait()
+    engine.stop()
+
+
+#Speak Good Morning
+def speak_morning():
+    engine = pyttsx3.init()
+    engine.say("Good Morning!")
+    engine.runAndWait()
+    engine.stop()
+
+
+#Speak Good Afternoon
+def speak_afternoon():
+    engine = pyttsx3.init()
+    engine.say("Good Afternoon!")
+    engine.runAndWait()
+    engine.stop()
+
+
+#Speak Good Evening
+def speak_evening():
+    engine = pyttsx3.init()
+    engine.say("Good Evening!")
+    engine.runAndWait()
+    engine.stop()
+
+#Speak How May I help
+def speak_help():
+    engine = pyttsx3.init()
+    engine.say("How May I help you?")
+    engine.runAndWait()
+    engine.stop()
+
+
+#Speak Do you want to add anything
+def speak_add():
+    engine = pyttsx3.init()
+    engine.say("Do you want too add anything?")
+    engine.runAndWait()
+    engine.stop()
+
+#Speak Thank you
+def speak_thanks():
+    engine = pyttsx3.init()
+    engine.say("Thank you!!")
+    engine.runAndWait()
+    engine.stop()
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
@@ -30,6 +85,28 @@ class App(ctk.CTk):
         # Button to exit the application
         self.ctrl_c_button = ctk.CTkButton(self, text="Exit", width=50, command=self.exit_with_ctrl_c)
         self.ctrl_c_button.place(x=800, y=160)
+
+        #Text to Speech Buttons
+        self.speak_button = ctk.CTkButton(self, text="Hello", width=70, command=speak_hello)
+        self.speak_button.place(x=580,y=220)
+
+        self.speak_button = ctk.CTkButton(self, text="Good Morning", width=70, command=speak_morning)
+        self.speak_button.place(x=580,y=260)
+
+        self.speak_button = ctk.CTkButton(self, text="Good Afternoon", width=70, command=speak_afternoon)
+        self.speak_button.place(x=580,y=300)
+
+        self.speak_button = ctk.CTkButton(self, text="Good Evening", width=70, command=speak_evening)
+        self.speak_button.place(x=580,y=340)
+
+        self.speak_button = ctk.CTkButton(self, text="How May I help you?", width=70, command=speak_help)
+        self.speak_button.place(x=580,y=380)
+
+        self.speak_button = ctk.CTkButton(self, text="Do you want too add anything?", width=70, command=speak_add)
+        self.speak_button.place(x=580,y=420)
+
+        self.speak_button = ctk.CTkButton(self, text="Thank You", width=70, command=speak_thanks)
+        self.speak_button.place(x=580,y=460)
 
     # Method to handle the exit button click (closes the window)
     def exit_with_ctrl_c(self):
